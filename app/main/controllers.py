d494aca78dcedda41a3144468211c07227915c8c
Russ Cox <russfcox@gmail.com>
2018-05-02 22:08:51 +0100

    updated text displayed on page

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -5,7 +5,7 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 def hello():
-    return "Hello World! v:1"
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080)
